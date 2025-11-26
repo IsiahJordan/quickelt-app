aee22c9cc3989495587811d2ba91a79f0aabb08b
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-26 14:46:41 +0800

    Create component inputbox

---
+++ app/src/components/InputBox/InputBox.tsx
@@ -0,0 +1,34 @@
+import { InputVariant, InputProps } from '@/types/inputbox.d.ts'
+
+const variantInputClasses: Record<InputVariant, string> = {
+  dark: "bg-accent-500/60",
+  light: "bg-accent-300",
+  outline: ""
+};
+
+export default function InputBox({ 
+  variant="dark", 
+  type="text",
+  color="text-default", 
+  placeholder,
+  leftChild,
+  rightChild,
+  onChange
+} : InputProps) {
+  const inputBase = "input font-default";
+  const boxBase = "box font-default";
+  const inputClasses = inputBase + " " + variantInputClasses[variant];
+  const boxClasses = boxBase + " " + color;
+  
+  return (
+    <div className={inputClasses}>
+      { leftChild && 
+        <div className="icon-input">{leftChild}</div>
+      }
+      <input className={boxClasses} type={type} placeholder={placeholder}/>
+      { rightChild &&
+        <div className="icon-input">{rightChild}</div>
+      }
+    </div>
+  );
+}
