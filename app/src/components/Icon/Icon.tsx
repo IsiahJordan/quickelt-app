ffe27bbd87afcda69e9fa007ccc09a835ea257ae
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-26 18:12:46 +0800

    Create icon component

---
+++ app/src/components/Icon/Icon.tsx
@@ -0,0 +1,21 @@
+import { IconVariant } from '@/types/icon.d.ts'
+import { IoMdMail } from "react-icons/io";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+export default function Icon({ variant }: IconVariant) {
+  
+  // in the future, add logic into specific icon in responds
+  // to niche use case
+  switch (variant) {
+    case "mail":
+      return <IoMdMail size={28} color="#ffffff" opacity="90%"/>;
+    case "lock":
+      return <FaLock size={22}/>;
+    case "neye":
+      return <FaRegEyeSlash size={28}/>;
+  }
+
+  // replace this with icon placeholder
+  return undefined;
+}
---
+++ app/src/types/icon.d.ts
@@ -0,0 +1,2 @@
+
+export type IconVariant = 'mail' | 'lock' | 'password';
