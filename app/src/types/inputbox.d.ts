7b431cad46191532fb75a5f2bb7527d096bc555c
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-26 14:47:04 +0800

    Create type for inputbox

---
+++ app/src/types/inputbox.d.ts
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export type InputVariant = 'dark' | 'light' | 'outline';
+
+export interface InputProps {
+  variant?: InputVariant;
+  type?: string;
+  color?: string;
+  placeholder: string;
+  leftChild?: React.ReactNode;
+  rightChild?: React.ReactNode;
+  onChange: (text: string) => void;
+};
