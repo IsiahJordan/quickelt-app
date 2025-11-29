cb8832d9f01f4f6fc2bdd3e5c6ed626fbbd5ec59
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-29 10:55:20 +0800

    Modify root to wrap BrowserRouter

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Log from '@/utils/log'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+
+const log = Log("GLOBAL");
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+      onError: (error: any) => {
+        log.danger(error);
+      }
+    }
+  },
+  mutations: {
+    onError: (error: any) => {
+      log.danger(error);
+    }
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
