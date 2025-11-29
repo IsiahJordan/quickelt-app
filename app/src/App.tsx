3470a34796dde4c67303b09799b706e336521106
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-29 10:35:40 +0800

    Add react routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,21 @@
-
+import Log from '@/utils/log'
+import { Routes, Route } from 'react-router-dom'
+import SignPage from '@/pages/SignPage'
+import DarkLayout from '@/layouts/DarkLayout'
 
 function App() {
+  const log = Log("App");
+  log.info("start app");
 
   return (
     <>
+      <Routes>
+        <Route element={<DarkLayout/>}>
+          <Route path="/sign" element={<SignPage/>}/>
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
