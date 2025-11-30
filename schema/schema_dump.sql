6a888d3a64e415ce79337ea38d69e89aa05b2e9b
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-30 15:12:11 +0800

    Dump shema for accounts

--- schema/schema_dump.sql
+++ schema/schema_dump.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
-\restrict NKCf6aFdjHpgHsIwAIEwguJX4kt3zbjM6NeM3rMNbeqNaxeaGd3UZGdiJgc0kg5
+\restrict JCPl63XuTOvs9ycim9fRhpizNR9dBfiPQbtc9EUlDukCJEBwRp8czbCSp4m4qnS
 
 -- Dumped from database version 18.0
--- Dumped by pg_dump version 18.0
+-- Dumped by pg_dump version 18.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,8 +20,71 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+--
+-- Name: role; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.role AS ENUM (
+    'user',
+    'admin'
+);
+
+
+ALTER TYPE public.role OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.accounts (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    username character varying(50) NOT NULL,
+    password character varying(255) NOT NULL,
+    email character varying(50) NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone,
+    role public.role DEFAULT 'user'::public.role
+);
+
+
+ALTER TABLE public.accounts OWNER TO postgres;
+
+--
+-- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: accounts uq_acc_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT uq_acc_email UNIQUE (email);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
-\unrestrict NKCf6aFdjHpgHsIwAIEwguJX4kt3zbjM6NeM3rMNbeqNaxeaGd3UZGdiJgc0kg5
+\unrestrict JCPl63XuTOvs9ycim9fRhpizNR9dBfiPQbtc9EUlDukCJEBwRp8czbCSp4m4qnS
 
