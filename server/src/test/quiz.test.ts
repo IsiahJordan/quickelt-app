01e8d092a6b9ce3af948f0b79b4ab45c604c32fe
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-11-30 15:14:36 +0800

    Create test suite for quiz list and create

---
+++ server/src/test/quiz.test.ts
@@ -0,0 +1,30 @@
+import app from '../app'
+import request from 'supertest'
+import path from 'path'
+
+describe("POST /create", () => {
+  it("should receive success and token with an image", async () => {
+    const res = await request(app)
+      .post("/data/quiz/create")
+      .field("name", "algebra")
+      .field("metadata", JSON.stringify({ tags: ["math","logic","puzzle"] }))
+      .attach("image", path.join(__dirname, "assets/book-test.jpg"));
+
+    expect(res.status).toBe(201);
+    expect(res.body).toBeDefined();
+    expect(res.body.success).toBe(true);
+  });
+});
+
+describe("GET /list", () => {
+  it('should reflect success and update table', async () => {
+    const res = await request(app).get('/data/quiz/list').send({
+      name: "algebra"      
+    });
+    
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.success).toBe(true);
+  });
+});
+
