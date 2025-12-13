ecfb575384127ea710020717f0e83221defb8613
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-13 14:34:10 +0800

    Modify to add router schema

--- api/service.py
+++ api/service.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from strawberry.fastapi import GraphQLRouter
 
 from .utils.file import read_json 
+from .routers.question import schema as question_schema
 
 config: dict = read_json("settings.json")
+
+question_router = GraphQLRouter(question_schema)
 
 app = FastAPI()
 app.add_middleware(
@@ -13,3 +17,5 @@ app.add_middleware(
     allow_methods=config["cors"]["methods"],
     allow_headers=config["cors"]["headers"]
 )
+
+app.include_router(question_router, prefix="/api/question")
