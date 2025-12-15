0a48c440cd1af2390a53e5639d345946489375f5
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-15 17:34:39 +0800

    Add tag query and fix quiz data

--- app/src/pages/QuizPage/QuizPage.tsx
+++ app/src/pages/QuizPage/QuizPage.tsx
@@ -4,72 +4,76 @@ import Icon from '@/components/Icon'
 import Log from '@/utils/log'
 
 import { useState, useEffect } from 'react'
-import { useQuizList } from '@/hooks/useQuiz'
+import { useQuizList, useTagList } from '@/hooks/useQuiz'
 import TableCard from '@/components/TableCard'
 
 export default function QuizPage() {
-  const tags = ["Science", "Computer Science", "Art"];
-  const log = Log("QuizPage");
-  const [input, setInput] = useState<string>("");
-  const [quizArr, setQuizArr] = useState([]);
-  const { data, isLoading, error } = useQuizList(1, 10); 
+  const log = Log('QuizPage');
+
+  const [input, setInput] = useState('');
+  const [tags, setTags] = useState<Record<string, 'active' | 'inactive'>>({});
+
+  const { data: quiz_data } = useQuizList(0, 10);
+  const { data: tag_data } = useTagList();
 
   useEffect(() => {
-    if (!data) return;
-    const arr = [];
-    for (let i = 0; i < data.length; i++) {
-      arr.push(
-          <TableCard
-            key={data[i].id}
-            styles={[
-              'sm:w-24 max-sm:w-12 mt-8 mx-2',
-              'max-sm:w-[35vw] sm:w-[40vw] mx-2',
-              'max-sm:w-[20vw] sm:flex-1',
-            ]}
-            onClick={() => log.debug(data[i].id) }
-          >
-            <Icon variant='image' color='text-default'/>
-            <h2>Computer Science</h2>
-            <h2>Isiah Jordan</h2>
-          </TableCard>
-      );
+    if (!tag_data) return;
+
+    const nextTags: Record<string, 'active' | 'inactive'> = {}
+    
+    for (const tag of tag_data) {
+      nextTags[tag.name] = 'inactive';
     }
-    setQuizArr(arr);
-  }, [data]);
 
-  if (isLoading) {
-    return <h1>Loading...</h1>
-  }
-  if (error) {
-    return <h1>Error...</h1>
-  }
-
+    setTags(nextTags);
+  }, [tag_data])
 
   return (
     <div className="flex flex-col sm:px-18 sm:py-6 gap-y-2">
       <h1 className="text-default">Quizzes</h1>
+
       <div className="sm:max-w-[40%]">
-        <InputBox 
+        <InputBox
           variant="outline"
           type="search"
           placeholder="Search..."
-          rightChild={<Icon variant="search"/>}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
+          rightChild={<Icon variant="search" />}
+          onChange={(e) => setInput(e.target.value)}
         />
       </div>
+
       <div className="flex gap-x-2 mt-1">
-        {tags.map((item, index) => (
-          <div key={index}>
-            <Badge
-              variant="inactive"
-              label={item}
-            />
-          </div>
+        {Object.keys(tags).map((key) => (
+          <Badge
+            key={key}
+            variant={tags[key]}
+            label={key}
+            onClick={() =>
+              setTags((prev) => ({
+                ...prev,
+                [key]: prev[key] === 'inactive' ? 'active' : 'inactive',
+              }))
+            }
+          />
         ))}
       </div>
-      {quizArr}
+
+      {quiz_data?.map((quiz) => (
+        <TableCard
+          key={quiz.id}
+          styles={[
+            'sm:w-24 max-sm:w-12 mt-8 mx-2',
+            'max-sm:w-[35vw] sm:w-[40vw] mx-2',
+            'max-sm:w-[20vw] sm:flex-1',
+          ]}
+          onClick={() => log.debug(quiz.id)}
+        >
+          <Icon variant="image" color="text-default" />
+          <h2>{quiz.name ?? 'Not Applicable'}</h2>
+          <h2>{quiz.metadata.author ?? 'Unknown'}</h2>
+        </TableCard>
+      ))}
     </div>
-  );
+  )
 }
+
