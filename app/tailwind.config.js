826c088c34f659de02e7b1f3dac39c1cf2c130eb
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-17 13:36:48 +0800

    Install postcss and tailwind/postcss to include Lato and Barlow

---
+++ app/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ["Barlow", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
