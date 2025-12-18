3ec2784de17945596d0844534b28b2ae8e379d23
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-18 18:24:48 +0800

    Fix route for auth

--- server/src/routes/helper.ts
+++ server/src/routes/helper.ts
@@ -3,6 +3,9 @@ import express from 'express'
 const router = express.Router();
 
 import { requireBody, requireParams } from '../middleware/validation.ts'
+import { validate } from '../controllers/helperController.ts'
+import { authToken, isAuthorize } from '../middleware/auth.ts'
 
+router.post("/fetch/token", authToken, isAuthorize, validate);
 
 export default router;
