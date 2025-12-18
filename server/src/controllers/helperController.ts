aea7a8638dfca8eacf6265445f1488c67635a0f5
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-18 18:26:29 +0800

    Create a validater to handle no controller routes

--- server/src/controllers/helperController.ts
+++ server/src/controllers/helperController.ts
@@ -1,3 +1,11 @@
 import Log from '../utility/log.ts'
 import path from 'path'
 
+// this is used for process pipeline
+// that doesn't use a controller
+// therefore, is a substitution for such
+export function validate(req, res) {
+  const log = Log("validate");
+
+  return res.status(200).json({success: true, message: "successful process"});
+}
