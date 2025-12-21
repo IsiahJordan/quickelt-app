14429170a63b8576a822ad7d7a321c0dc0b84efc
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-21 23:07:22 +0800

    Create a question page

---
+++ app/src/pages/QuestionPage/QuestionPage.tsx
@@ -0,0 +1,54 @@
+import Log from '@/utils/log'
+import Button from '@/components/Button'
+
+import { useParams } from 'react-router-dom'
+import { useQuestionList } from '@/hooks/useQuestion'
+import { useState, useEffect } from 'react'
+
+export default function QuestionPage() {
+  const log = Log("QuestionPage");
+  const { id: quizId } = useParams();
+  const { data: questions_data } = useQuestionList(quizId);
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    if (!questions_data) return;
+    const arr = new Array(questions_data.length);
+    setAnswers(arr);
+  }, [questions_data]);
+
+  return (
+    <div className="flex items-center flex-col w-[60%] max-w-[46rem] mx-auto my-6">
+      {questions_data?.map((item, index) => (
+        <div key={index} className="w-[100%]">
+          <p className="max_sm:text-lg sm:text-2xl tracking-wide font-semibold mb-2">Question {index + 1}</p>
+          <p className="max_sm:text-[14px] sm:text-xl font-light">{item.description}</p>
+          
+          <div className="sm:grid sm:grid-cols-2 sm:grid-rows-2 max_sm:flex max_sm:flex-col gap-4 w-[100%] py-2">
+            {item.options.map((option, indexes) => (
+              <div key={indexes} className="w-[100%] py-1">
+                <Button
+                  variant={"nofill"}
+                  label={
+                    `${String.fromCharCode('A'.charCodeAt(0) + indexes)}. ${option}`
+                  }
+                  color="text-default-alt"
+                  style="text-[12px] font-thin max-sm:min-h-12 sm:min-h-14 rounded-none"
+                  onClick={() => {
+                    log.debug(`indexes ${index}, ${indexes}`);
+                    
+                    const arr = answers;
+                    arr[index] = indexes;
+                    setAnswers(arr);
+
+                    log.debug(arr);
+                  }}
+                />
+              </div> 
+            ))}
+          </div>
+        </div>
+      ))} 
+    </div>
+  ); 
+}
