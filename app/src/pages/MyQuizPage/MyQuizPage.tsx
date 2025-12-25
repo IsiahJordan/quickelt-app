a7ca7f79b7c3b9b7624e5562bcbfe51c3f6cf4d8
Isiah Jordan <dimaunahanisiahjordan@gmail.com>
2025-12-25 22:22:44 +0800

    Fix image and description bug

--- app/src/pages/MyQuizPage/MyQuizPage.tsx
+++ app/src/pages/MyQuizPage/MyQuizPage.tsx
@@ -2,10 +2,13 @@ import InputBox from '@/components/InputBox'
 import Badge from '@/components/Badge'
 import Icon from '@/components/Icon'
 import Log from '@/utils/log'
+import Button from '@/components/Button'
 
 import { useState, useEffect } from 'react'
 import { useMyQuizList } from '@/hooks/useCreate'
+import { useAccount } from '@/hooks/useAccount'
 import { getImage } from  '@/services/helper/helper.api.ts'
+import { useCreateQuiz } from '@/hooks/useQuiz'
 import useNav from '@/hooks/useNav';
 import TableCard from '@/components/TableCard'
 
@@ -14,12 +17,30 @@ export default function MyQuizPage() {
 
   const [input, setInput] = useState('');
   const quizMutation = useMyQuizList();
-  const { setQuery } = useNav();
+  const accountMutation = useAccount();
+  const createMutation = useCreateQuiz();
 
   useEffect(() => {
+    accountMutation.mutate();
     quizMutation.mutate();
+    
     log.debug(quizMutation.data);
   }, []);
+
+  const handleCreate = () => {
+    log.debug("create called");
+    log.debug(JSON.stringify(accountMutation.data.data)); 
+    
+    createMutation.mutate({
+      name: "No Title",
+      metadata: {
+        author: accountMutation.data.data.email.split("@")[0],
+        description: "No Description",
+        duration: 60
+      }
+    });
+
+  };
 
   return (
     <div className="flex flex-col sm:px-18 sm:py-6 gap-y-2">
@@ -34,6 +55,23 @@ export default function MyQuizPage() {
           onChange={(e) => setInput(e.target.value)}
         />
       </div>
+      
+      <div className="sm:max-w-52 max-sm:max-w-46 pt-2">
+        <Button
+          variant="secondary"
+          label={(
+            <div className="text-default flex text-lg">
+              Create Quiz
+              <div className="pt-1 pl-1">
+                <Icon variant="add" color="text-default"/>
+              </div>
+            </div>
+          )}
+          style="flex items-center sm:h-12 max-sm:h-10 max-sm:text-medium justify-center"
+          onClick={handleCreate}
+        />
+      </div>
+
 
       {quizMutation.data?.map((quiz) => (
         <TableCard
@@ -45,12 +83,13 @@ export default function MyQuizPage() {
           ]}
           onClick={() => setQuery({view: quiz.id})}
         >
-          {<img
+          { quiz.image_url ?
+            <img
             src={getImage({imageUrl: quiz.image_url })}
             alt={quiz.image_url}
-          /> ?? <Icon variant="image" color="text-default" />}
+          /> : <Icon variant="image" color="text-default" />}
           <h2>{quiz.name ?? 'Not Applicable'}</h2>
-          <h2>{quiz.metadata.author ?? 'Unknown'}</h2>
+          <h2>{quiz.metadata.description ?? 'No Description'}</h2>
         </TableCard>
       ))}
     </div>
